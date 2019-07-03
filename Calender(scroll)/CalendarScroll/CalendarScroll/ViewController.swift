ded99645434efc686b265893bbbf0f86e71afd19
JUNHYEOK KWON <>
2019-07-03 21:33:55 +0900

    스크롤 보정 기능 추가

--- Calender(scroll)/CalendarScroll/CalendarScroll/ViewController.swift
+++ Calender(scroll)/CalendarScroll/CalendarScroll/ViewController.swift
@@ -9,13 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet private weak var collectionView: UICollectionView!
+    private var zeroCell: CollectionViewCell?
+    private var cellWidth: Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
 
+    }
+
+    private func scrollingCorrection() {
+        let x =  Int(collectionView.contentOffset.x)
+        let x70 = Int(x / cellWidth) * cellWidth
+        if x % cellWidth > cellWidth / 2 {
+            collectionView.setContentOffset(CGPoint(x: x70 + cellWidth, y: 0), animated: false)
+        } else {
+            collectionView.setContentOffset(CGPoint(x: x70, y: 0), animated: false)
+        }
     }
 
 }
@@ -33,14 +45,17 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
         cell.layer.borderWidth = 0.5
         cell.layer.borderColor = UIColor.black.cgColor
-        if indexPath.section == 0 {
-            cell.backgroundColor = .gray
-        } else {
-            cell.backgroundColor = .red
-        }
-
         cell.label.text = "\(indexPath.section)"
+        cellWidth = Int(cell.frame.width)
         return cell
+    }
+
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        scrollingCorrection()
+    }
+
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        scrollingCorrection()
     }
 
 }
